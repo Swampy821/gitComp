49e3b6433d6c0593dd3261e779088af921ee835a
Steve Marsh <Zimdale@gmail.com>
2014-10-25 01:06:57 -0400

    Got firebase working

--- frontend/dev/js/Controllers/mainApp.js
+++ frontend/dev/js/Controllers/mainApp.js
@@ -1,6 +1,47 @@
+(function() {
+	function git() {}
 
-myApp.controller('mainApp', ['$scope', '$firebase',
-        function($scope, $firebase) {
+	git.prototype.averageCommits = function(repoArray) {
+
+	}
+
+	window.git = new git();
+})();
+
+
+
+
+
+myApp.controller('mainApp', ['$scope', '$firebase', '$http',
+        function($scope, $firebase, $http) {
 	var ref = new Firebase('https://amber-torch-2607.firebaseio.com/users/');
-	$scope.users = $firebase(ref).$asArray();
+	var fire = $firebase(ref);
+	$scope.users = fire.$asArray(); 
+	$scope.getGitData = function(username, index) {
+		var timeStamp = new Date().getTime();
+		if(timeStamp > $scope.users[index].timeStamp + 200000 || $scope.users[index].timeStamp === undefined) {
+			$http.get('https://api.github.com/users/' + username + '/repos')
+			  .success(function(data) {
+			  	$scope.users[index].repoCount = data.length;
+			  	$scope.users[index].timeStamp = timeStamp;
+			  	$scope.users.$save(index);
+			  	console.log('update called on index ' + index);
+			  });
+		}
+	};
+	$scope.update = function() {
+		for(var i=0; i<$scope.users.length; i++) {
+			$scope.getGitData($scope.users[i].username, i);
+		}
+	};
+	$scope.users.$loaded().then(function() {
+		$scope.update();
+	});
+
+	$scope.$watch('state.start', function(oldVal, newVal) {
+		if(newVal===true) {
+			$scope.update();
+		}
+	});
+
 }]);
