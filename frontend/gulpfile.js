9050a9fdfeac40af5912e6cc3428354629db83f0
Steve Marsh <Zimdale@gmail.com>
2014-10-24 22:58:42 -0400

    Got base design and stuff working

---
+++ frontend/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+gulp.task('default',['js','css', 'copy']);
+
+
+gulp.task('js', function() {
+  gulp.src(
+  	['./dev/js/app.js', 
+  	 './dev/js/Controllers/home.js', 
+  	 './dev/material/js/material.min.js', 
+  	 './dev/material/js/ripples.min.js'])
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('./build/'))
+});
+
+
+gulp.task('css', function() {
+  gulp.src('./dev/css/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dev/css'));
+
+  gulp.src(
+  	['./dev/material/css/material-wfont.min.css', 
+  	 './dev/material/css/material.min.css', 
+  	 './dev/material/css/ripples.min.css',
+     './dev/css/style.css'])
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('copy', function() {
+  gulp.src('dev/index.html')
+    .pipe(gulp.dest('build'));
+});
